10147258585f739c550eb262a01a71ffd39e36c9
[夜 <[tql163.love@163.com]>
2018-02-23 09:43:52 +0800

    添加

--- react-dome/src/Routes/routes.jsx
+++ react-dome/src/Routes/routes.jsx
@@ -20,11 +20,11 @@ class AppMain extends React.Component {
 
 let routes = (
 	<div>
-	<Route path="/" component={App}/> 
-	<IndexRoute   component={App} />
-	// 剩下的问题，应该是这里的问题，这里你组件嵌套重复了，在用一个空壳子啥的包一下，应该就可以了，88
-	<Route path="goods" component={Goods} />
-	<Route path="goods/:id" component={Goods} />
+		<Route path="/" component={App}/> 
+		<IndexRoute   component={App} />
+		// 剩下的问题，应该是这里的问题，这里你组件嵌套重复了，在用一个空壳子啥的包一下，应该就可以了，88
+		<Route path="goods" component={Goods} />
+		<Route path="goods/:id" component={Goods} />
 	</div>
 );
 
